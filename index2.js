04f96ebb73955625cf7e7788eb83003ccf3fa9b2
ShuCheng56 <shucheng56@gmail.com>
2022-01-29 15:21:59 -0600

    javaexercise

---
+++ index2.js
@@ -0,0 +1,8 @@
+var firstName,lastName,age,dateOfBirth,address;
+firstName="Shu";
+LastName="Cheng";
+age=1;
+dateOfBirth="01/-01/2000";
+address="111 Saskatoon";
+console.log(firstName,lastName,age,dateOfBirth,address);
+
---
+++ index5.js
@@ -0,0 +1,14 @@
+
+var firstName="Shu";
+var LastName="Cheng";
+var age=1;
+var dateOfBirth="01/-01/2000";
+var address="111 Saskatoon";
+console.log(age);
+
+
+
+
+
+
+
